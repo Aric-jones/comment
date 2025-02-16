713a6ed0c30a6bb185cfb0faf014e0788f0f3545
chensihua <2926480799@qq.com>
2025-02-16 16:34:13 +0800

    fix:查询店铺信息设置缓存穿透逻辑

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -2,11 +2,11 @@ package com.hmdp.service.impl;
 
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -42,23 +42,30 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         String shopValue = redisTemplate.opsForValue().get(shopKey);
 
         // 2. 判断是否存在
-        if(StringUtils.isNotBlank(shopValue)){
+        if (StringUtils.isNotBlank(shopValue)) {
             // 3. 存在直接返回
             return Result.ok(JSONUtil.toBean(shopValue, Shop.class));
         }
 
-        // 4. 不存在，根据id查询数据库
-        Shop shop = this.getById(id);
-
-        // 5. 查询不到数据
-        if(Objects.isNull(shop)){
+        // 4. 判断是否是空对象
+        if (RedisConstants.CACHE_NULL_VALUE.equals(shopValue)) {
             return Result.fail("店铺不存在");
         }
 
-        // 6. 将查询到的数据保存到redis中
+        // 5. 不存在，根据id查询数据库
+        Shop shop = this.getById(id);
+
+        // 6. 查询不到数据
+        if (Objects.isNull(shop)) {
+            // 缓存空对象，解决缓存穿透问题
+            redisTemplate.opsForValue().set(shopKey, RedisConstants.CACHE_NULL_VALUE, RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return Result.fail("店铺不存在");
+        }
+
+        // 7. 将查询到的数据保存到redis中
         redisTemplate.opsForValue().set(shopKey, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
-        // 7. 返回
+        // 8. 返回
         return Result.ok(shop);
     }
 
